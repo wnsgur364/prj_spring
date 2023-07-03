c59abb187359c7009c807e72d6fb7793a4651f1b
wnsgur364 <wnsgur364@naver.com>
2023-07-03 21:27:19 +0900

    1

---
+++ src/main/java/com/mycompany/app/infra/modules/code/CodeController.java
@@ -0,0 +1,58 @@
+package com.mycompany.app.infra.modules.code;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mycompany.app.infra.modules.codegroup.CodeGroupServiceImpl;
+import com.mycompany.app.infra.modules.codegroup.CodeGroupVo;
+
+@Controller
+public class CodeController {
+
+	@Autowired
+	CodeServiceImpl service;
+	@Autowired
+	CodeGroupServiceImpl groupService;
+	
+	@RequestMapping("/codeList")
+	public String codeList(@ModelAttribute("vo") CodeVo vo, Model model) {
+		vo.setShKeyword(vo.getShKeyword() == null ? "code" : vo.getShKeyword());
+		model.addAttribute("list", service.selectList(vo));
+		return "xdm/infra/code/codeList";
+	}
+	
+	@RequestMapping("/codeForm")
+	public String codeForm(CodeVo vo, Model model, CodeGroupVo groupvo, Model groupModel) {
+		model.addAttribute("item", service.selectOne(vo));
+		groupModel.addAttribute("group", groupService.selectList(groupvo));
+		return "xdm/infra/code/codeForm";
+	}
+	
+	@RequestMapping("/codeUpdate")
+	public String codeUpdate(Code dto) {
+		service.update(dto);
+		return "redirect:/codeList";
+	}
+	
+	@RequestMapping("/codeDelete")
+	public String codeDelete(Code dto) {
+		service.delete(dto);
+		return "redirect:/codeList";
+	}
+	
+	@RequestMapping("/codeInsert")
+	public String codeInsert(Code dto) {
+		service.insert(dto);
+		return "redirect:/codeList";
+	}
+	
+	@RequestMapping("/codeUelete")
+	public String codeUelete(Code dto) {
+		service.uelete(dto);
+		return "redirect:/codeList";
+	}
+	
+}
---
+++ src/main/java/com/mycompany/app/infra/modules/code/CodeDao.java
@@ -0,0 +1,32 @@
+package com.mycompany.app.infra.modules.code;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CodeDao {
+
+	@Inject
+	@Resource(name="sqlSession")
+	private SqlSession sqlSession;
+	
+	private static String namespace = "com.mycompany.app.infra.modules.code.CodeMapper";
+	
+	public List<Code> selectList(CodeVo vo) { return sqlSession.selectList(namespace + ".selectList", vo); }
+	
+	public Code selectOne(CodeVo vo) { return sqlSession.selectOne(namespace + ".selectOne", vo); }
+	
+	public int update(Code dto) { return sqlSession.update(namespace + ".update", dto); }
+	
+	public int delete(Code dto) { return sqlSession.delete(namespace + ".delete", dto); }
+	
+	public int uelete(Code dto) { return sqlSession.update(namespace + ".uelete", dto); }
+	
+	public int insert(Code dto) { return sqlSession.insert(namespace + ".insert", dto); }
+	
+}
