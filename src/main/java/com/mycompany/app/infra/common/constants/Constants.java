37efb905257ef902023ef0dadc679f350a16b678
wnsgur364 <wnsgur364@naver.com>
2023-07-26 14:37:51 +0900

    UtilDateTime Class 추가

--- src/main/java/com/mycompany/app/infra/common/constants/Constants.java
+++ src/main/java/com/mycompany/app/infra/common/constants/Constants.java
@@ -14,4 +14,9 @@ public final class Constants {
 	public static final String UPLOAD_PATH_PREFIX_EXTERNAL = "D://factory/ws_eclipse/prj_spring/src/main/webapp";
 	public static final String UPLOAD_PATH_PREFIX = "D://factory/ws_eclipse/prj_spring/src/main/webapp/resources/uploaded";
 	public static final String UPLOAD_PATH_PREFIX_FOR_VIEW = "/resources/uploaded";
+
+//	UtilDateTime
+	public static final String DATETIME_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
+	public static final String TIME_FORMAT_BASIC = "HH:mm:ss";
+	
 }
